7905c0e25f4e41ef4d490461e06945471b8f0d38
PengjieJCU <jie.peng@my.jcu.edu.au>
2019-03-26 22:59:38 +1000

    Initial commit

---
+++ app/src/main/java/au/edu/jcu/cp3406/temperatureconverter/MainActivity.java
@@ -0,0 +1,20 @@
+package au.edu.jcu.cp3406.temperatureconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void buttonClicked(View view){
+        ///
+    }
+
+
+
+}
