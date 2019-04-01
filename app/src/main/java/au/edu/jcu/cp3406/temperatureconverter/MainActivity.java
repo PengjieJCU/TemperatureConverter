d4e432662de075e98b8200ee20c0bdc77c1d1a25
PengjieJCU <jie.peng@my.jcu.edu.au>
2019-04-01 17:03:02 +1000

    Change the Linear Layout to Relative Layout, and add two spinners and textviews.

--- app/src/main/java/au/edu/jcu/cp3406/temperatureconverter/MainActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/temperatureconverter/MainActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -13,15 +15,17 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private Spinner spinner;
+    private Spinner spinner2;
     ArrayAdapter <String> adapter;
     private List<String> list;
+    Integer Celsius;
+    Integer Fahrenheit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         spinner=(Spinner)findViewById(R.id.spinner);
-        spinner=(Spinner)findViewById(R.id.spinner2);
         spinner.setPrompt("Please choose a unit");
         initDatas();
         adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list);
@@ -37,7 +41,24 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        spinner2=(Spinner)findViewById(R.id.spinner2);
+        spinner2.setPrompt("Please choose a unit");
+        initDatas();
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list);
+        spinner2.setAdapter(adapter);
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,"The unit you choose is:"+list.get(position),Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
+
+
     private void initDatas(){
         list=new ArrayList<String>();
         list.add("Celsius");
@@ -45,8 +66,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buttonClicked(View view){
-        ///
+        Button button = (Button) view;
+        int Fahrenheit = Integer.parseInt(button.getText().toString());
+
+        Celsius = (Fahrenheit-32)*5/9;
+        TextView textView = findViewById(R.id.Fahrenheit);
+        textView.setText(Fahrenheit);
+
+
+
     }
+
 
 
 
