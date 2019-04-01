75bc2f3eab56b1587fc1addd6ef890ae32bb2e99
PengjieJCU <jie.peng@my.jcu.edu.au>
2019-04-01 13:49:12 +1000

    Change the Linear Layout to Relative Layout, and add two spinners and textviews.

--- app/src/main/java/au/edu/jcu/cp3406/temperatureconverter/MainActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/temperatureconverter/MainActivity.java
@@ -3,14 +3,47 @@ package au.edu.jcu.cp3406.temperatureconverter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner spinner;
+    ArrayAdapter <String> adapter;
+    private List<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner=(Spinner)findViewById(R.id.spinner);
+        spinner=(Spinner)findViewById(R.id.spinner2);
+        spinner.setPrompt("Please choose a unit");
+        initDatas();
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,"The unit you choose is:"+list.get(position),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
+    private void initDatas(){
+        list=new ArrayList<String>();
+        list.add("Celsius");
+        list.add("Fahreheit");
+    }
+
     public void buttonClicked(View view){
         ///
     }
